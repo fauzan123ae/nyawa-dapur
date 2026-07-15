8c84cb8c86890577446336dbd07a2d92da51f630
Fauzan Fathin Zaky <fauzanfathinzaky357@gmail.com>
2026-07-15 10:04:45 +0700

    fix auth

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -1,5 +1,5 @@
 import client from './client'
-export const login    = (data) => client.post('/login', data)
-export const register = (data) => client.post('/register', data)
-export const logout   = ()     => client.post('/logout')
-export const getMe    = ()     => client.get('/me')
+export const login    = (data) => client.post('/auth/login', data)
+export const register = (data) => client.post('/auth/register', data)
+export const logout   = ()     => client.post('/auth/logout')
+export const getMe    = ()     => client.get('/auth/me')
