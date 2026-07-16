3e31a1dbc2e153c059531245c3745ea1030565cf
Fauzan Fathin Zaky <fauzanfathinzaky357@gmail.com>
2026-07-16 15:19:16 +0700

    fix route

--- server/src/routes/wasteHistory.js
+++ server/src/routes/wasteHistory.js
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { authenticate } from '../middleware/auth.js'
-import { getWasteHistory, deleteWasteHistory } from '../controllers/Wastehistorycontroller .js'
+import { getWasteHistory, deleteWasteHistory } from '../controllers/wasteHistoryController.js'
 
 const router = Router()
 router.get('/',      authenticate, getWasteHistory)
